8d14ce5a522e66b8f87ade8be597f62f1581d5cb
Zach Boone <zach@predictspring.com>
2019-07-17 00:18:00 -0700

    separate out demo blurbs

--- routes/demoRouter.js
+++ routes/demoRouter.js
@@ -4,14 +4,28 @@ const demoRouter = express.Router();
 const demos = require("./demos");
 const pages = require("./pages");
 
-demoRouter.get("*", (req, res, next) => {
-  const path = req.path.substring("/demos/".length);
+const run = (req, res, next) => {
+  const path = req.path.substring("/demos/run/".length);
   const demo = demos.find(d => d.path === path);
   if (demo) {
-    res.render("demo", { pages, page: demo });
+    res.render("demo", { pages, page: demo, isDemo: true });
   } else {
     next();
   }
-});
+};
+
+const blurbs = (req, res, next) => {
+  const path = req.path.substring("/demos/blurbs/".length);
+  const demo = demos.find(d => d.path === path);
+  if (demo) {
+    res.render(demo.path, { pages, page: demo });
+  } else {
+    next();
+  }
+};
+
+demoRouter.get("/demos/run/*", run);
+
+demoRouter.get("/demos/blurbs/*", blurbs);
 
 module.exports = demoRouter;
--- routes/demos.js
+++ routes/demos.js
@@ -2,6 +2,8 @@ module.exports = [
   {
     path: "quick-sort",
     title: "Quick Sort",
+    description:
+      "a React application that helps you visualize and understand the quick sort algorithm",
     demoUrl: "https://storage.googleapis.com/jzachb-demos/quick-sort/index.html"
   }
 ];
