982a1e90ee9557974791e396dd84bea0f31fa9cb
Zach Boone <zach@predictspring.com>
2019-07-17 10:14:06 -0700

    my story

--- routes/pages.js
+++ routes/pages.js
@@ -10,13 +10,13 @@ module.exports = [
     title: "About"
   },
   {
-    path: "/skills",
-    template: "skills",
-    title: "Skills"
-  },
-  {
     path: "/demos",
     template: "demos",
     title: "Demos"
+  },
+  {
+    path: "/blog",
+    template: "blog",
+    title: "Blog"
   }
 ];
