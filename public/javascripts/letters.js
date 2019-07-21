f8137e3e1a75d69f2cf064da53057f43513055c6
Zach Boone <zach@predictspring.com>
2019-07-21 15:18:18 -0700

    console letters wip

---
+++ public/javascripts/letters.js
@@ -0,0 +1,57 @@
+const letters = {
+  a: [
+    "        ",
+    "        ",
+    "        ",
+    "XXXXXXXX",
+    "XX    XX",
+    "XX    XX",
+    "XX    XX",
+    "XXXXXXXX",
+    "      XX"
+  ],
+  b: [
+    "XX      ",
+    "XX      ",
+    "XX      ",
+    "XXXXXXXX",
+    "XX    XX",
+    "XX    XX",
+    "XX    XX",
+    "XXXXXXXX",
+    "       "
+  ],
+  c: [
+    "        ",
+    "        ",
+    "        ",
+    "XXXXXXXX",
+    "XX      ",
+    "XX      ",
+    "XX      ",
+    "XXXXXXXX",
+    "       "
+  ],
+  d: [
+    "      XX",
+    "      XX",
+    "      XX",
+    "XXXXXXXX",
+    "XX    XX",
+    "XX    XX",
+    "XX    XX",
+    "XXXXXXXX",
+    "       "
+  ],
+  e: [
+    "        ",
+    "        ",
+    "        ",
+    "XXXXXXXX",
+    "XX    XX",
+    "XXXXXXXX",
+    "XX      ",
+    "XXXXXXXX",
+    "       "
+  ]
+};
